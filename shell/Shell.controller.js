3c778b74bf37487541ce570fc26e8054dbe276e4
Igor Colyaskin <igor.colyaskin@gmail.com>
2026-01-17 18:56:16 +0400

    remove unused code

--- shell/Shell.controller.js
+++ shell/Shell.controller.js
@@ -42,7 +42,7 @@ sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel'], (Co
       if (oTelepath) {
         oTelepath.setHost(this._oHost)
       }
-      
+
       const oHostModel = new JSONModel({
         selectedId: 'Сигнала нет...',
       })
@@ -69,49 +69,6 @@ sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel'], (Co
         console.log("Оракул: Связь установлена, модель 'host' передана.")
       } else {
         console.error("Оракул не найден! Проверь ID='oracleCard' в Shell.view.xml")
-      }
-    },
-
-    /**
-     * Обработчик событий от карточек.
-     * Настроен в Shell.view.xml через action="onCardAction"
-     */
-    onCardAction: function (oEvent) {
-      const sActionType = oEvent.getParameter('type')
-      const oParameters = oEvent.getParameter('parameters')
-
-      // Мы реагируем только на наш кастомный тип действия
-      if (sActionType === 'Custom' && oParameters.selectedId) {
-        const sId = oParameters.selectedId
-        console.log(`Shell: Поймал клик! Передаю ID ${sId} в Miracle Card`)
-
-        this._updateDetailsCard(sId)
-      }
-    },
-
-    /**
-     * Внутренний метод для связи карточек
-     */
-    _updateDetailsCard: function (sId) {
-      // 1. Находим объект детальной карточки по ID из View
-      const oMiracleCard = this.getView().byId('miracleCard')
-
-      if (oMiracleCard) {
-        // 2. Пробираемся к внутреннему контроллеру карточки
-        // Сначала получаем Component, потом RootControl (View), потом Controller
-        const oCardContent = oMiracleCard.getCardContent()
-
-        if (oCardContent && oCardContent.getComponentInstance) {
-          const oComponent = oCardContent.getComponentInstance()
-          const oController = oComponent.getRootControl().getController()
-
-          // 3. Вызываем метод загрузки данных во второй карточке
-          if (oController && typeof oController.loadTaskDetails === 'function') {
-            oController.loadTaskDetails(sId)
-          }
-        } else {
-          console.warn('Shell: Miracle Card еще не загрузила контент.')
-        }
       }
     },
   })
