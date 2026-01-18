84e336b54d89006a647ea2aa9e9fe956215d3a0e
Igor Colyaskin <igor.colyaskin@gmail.com>
2026-01-18 23:50:21 +0400

    create archer Card for Custom Actions (Events)

---
+++ cards/archerActions/controller/ArcherActions.js
@@ -0,0 +1,11 @@
+sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/m/StandardListItem'], function (Controller, StandardListItem) {
+  'use strict'
+
+  return Controller.extend('com.epic.cards.archerActions.controller.ArcherActions', {
+    onInit: function () {
+    },
+
+    _loadData: function () {
+    },
+  })
+})
